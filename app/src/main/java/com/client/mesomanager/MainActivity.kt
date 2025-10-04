8239602d0ad271f0642b2bf81f73246e0706bd16
Christian Nytra <ch.nytra.13@gmail.com>
2025-10-04 18:01:47 +0200

    Navigation Boilerplate fertig

--- app/src/main/java/com/client/mesomanager/MainActivity.kt
+++ app/src/main/java/com/client/mesomanager/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.client.mesomanager.ui.composables.BottomNavigationBar
 import com.client.mesomanager.ui.theme.MesomanagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +17,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MesomanagerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                BottomNavigationBar()
             }
         }
     }
--- app/src/main/java/com/client/mesomanager/ui/composables/BottomNavigationBar.kt
+++ app/src/main/java/com/client/mesomanager/ui/composables/BottomNavigationBar.kt
@@ -1,47 +1,65 @@
 package com.client.mesomanager.ui.composables
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.client.mesomanager.ui.screens.ExercisesScreen
+import com.client.mesomanager.ui.screens.MesocyclesScreen
+import com.client.mesomanager.ui.screens.MoreScreen
+import com.client.mesomanager.ui.screens.WorkoutScreen
+import com.client.mesomanager.ui.theme.MesomanagerTheme
 
 @Composable
-fun BottomNavigationBar() {
+fun BottomNavigationBar(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
     val startDestination = Destination.WORKOUT
     var selectedDestination by rememberSaveable { mutableIntStateOf(startDestination.ordinal) }
 
-    NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) {
-        Destination.entries.forEachIndexed { index, destination ->
-            NavigationBarItem(
-                selectedDestination == index,
-                onClick = {
-                    navController.navigate(route = destination.route)
-                    selectedDestination = index
-                },
-                icon = {
-                    Icon(destination.icon, contentDescription = destination.description)
-                },
-                label = {
-                    destination.label
+    Scaffold(
+        modifier = modifier,
+        bottomBar = {
+            NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) {
+                Destination.entries.forEachIndexed { index, destination ->
+                    NavigationBarItem(
+                        selectedDestination == index,
+                        onClick = {
+                            navController.navigate(route = destination.route)
+                            selectedDestination = index
+                        },
+                        icon = {
+                            Icon(destination.icon, contentDescription = destination.description)
+                        },
+                        label = {
+                            destination.label
+                        }
+                    )
                 }
-            )
-        }
-        //NavigationBarItem()
+            }
+        }) { contentPadding ->
+        AppNavHost(navController, startDestination, modifier = Modifier.padding(contentPadding))
     }
+
 }
 
 enum class Destination(
@@ -57,7 +75,33 @@ enum class Destination(
 }
 
 @Composable
-@Preview
+fun AppNavHost(
+    navController: NavHostController,
+    startDestination: Destination,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController,
+        startDestination = startDestination.route
+    ) {
+        Destination.entries.forEach { destination ->
+            composable(destination.route) {
+                when (destination) {
+                    Destination.WORKOUT -> WorkoutScreen()
+                    Destination.MESOCYCLES -> MesocyclesScreen()
+                    Destination.EXERCISES -> ExercisesScreen()
+                    Destination.MORE -> MoreScreen()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
 fun NavigationPreview() {
-    BottomNavigationBar()
+    MesomanagerTheme(darkTheme = true) {
+        BottomNavigationBar()
+    }
 }
