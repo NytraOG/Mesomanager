a31d0ec5438732c464added66fd1d1d2448b3722
Christian Nytra <ch.nytra.13@gmail.com>
2025-10-04 17:45:24 +0200

    basic navigation done

--- app/src/main/java/com/client/mesomanager/ui/composables/BottomNavigationBar.kt
+++ app/src/main/java/com/client/mesomanager/ui/composables/BottomNavigationBar.kt
@@ -1,24 +1,63 @@
 package com.client.mesomanager.ui.composables
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun BottomNavigationBar(){
-    //var selectedDestination by rememberSaveable { mutableIntStateOf(startDestination.ordinal) }
+fun BottomNavigationBar() {
+    val navController = rememberNavController()
+    val startDestination = Destination.WORKOUT
+    var selectedDestination by rememberSaveable { mutableIntStateOf(startDestination.ordinal) }
 
     NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) {
+        Destination.entries.forEachIndexed { index, destination ->
+            NavigationBarItem(
+                selectedDestination == index,
+                onClick = {
+                    navController.navigate(route = destination.route)
+                    selectedDestination = index
+                },
+                icon = {
+                    Icon(destination.icon, contentDescription = destination.description)
+                },
+                label = {
+                    destination.label
+                }
+            )
+        }
         //NavigationBarItem()
     }
 }
 
+enum class Destination(
+    val route: String,
+    val label: String,
+    val icon: ImageVector,
+    val description: String
+) {
+    WORKOUT("workout", "Workout", Icons.Default.PlayArrow, "View current Workout"),
+    MESOCYCLES("mesocycles", "Mesocycles", Icons.Default.DateRange, "Create and view Mesocycles"),
+    EXERCISES("exercises", "Exercises", Icons.Default.Edit, "Create and view Exercises"),
+    MORE("more", "More", Icons.Default.Menu, "More Options"),
+}
+
 @Composable
 @Preview
-fun NavigationPreview(){
+fun NavigationPreview() {
     BottomNavigationBar()
 }
