529b9646dc95e0987f3e570067ce7ca96c4b7262
Christian Nytra <ch.nytra.13@gmail.com>
2025-10-04 21:51:01 +0200

    db singleton rein

--- app/src/main/java/com/client/mesomanager/MainActivity.kt
+++ app/src/main/java/com/client/mesomanager/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.room.Room
 import com.client.mesomanager.data.AppDatabase
+import com.client.mesomanager.data.Database
 import com.client.mesomanager.data.entities.Exercise
 import com.client.mesomanager.ui.composables.BottomNavigationBar
 import com.client.mesomanager.ui.theme.MesomanagerTheme
@@ -17,19 +18,6 @@ import com.client.mesomanager.ui.theme.MesomanagerTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val database = Room.databaseBuilder(
-            applicationContext,
-            AppDatabase::class.java,
-            "Mesomanager-db"
-        ).build()
-
-        val exerciseDao = database.exerciseDao()
-        val exercise = Exercise(0, "Curls")
-
-        exerciseDao.insert(exercise)
-
-        val reload = exerciseDao.get(0)
-
         enableEdgeToEdge()
         setContent {
             MesomanagerTheme {
--- app/src/main/java/com/client/mesomanager/data/AppDatabase.kt
+++ app/src/main/java/com/client/mesomanager/data/AppDatabase.kt
@@ -1,6 +1,8 @@
 package com.client.mesomanager.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.client.mesomanager.data.daos.ExerciseDao
 import com.client.mesomanager.data.entities.Exercise
@@ -8,4 +10,19 @@ import com.client.mesomanager.data.entities.Exercise
 @Database(entities = [Exercise::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun exerciseDao(): ExerciseDao
+}
+
+object Database {
+    @Volatile
+    private var INSTANCE: AppDatabase? = null
+
+    fun getInstance(context: Context): AppDatabase {
+        return INSTANCE ?: synchronized(this) {
+            INSTANCE ?: Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "Mesomanager-db"
+            ).build().also { INSTANCE = it }
+        }
+    }
 }
