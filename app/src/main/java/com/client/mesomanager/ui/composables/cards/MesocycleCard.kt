b952859fd0bb7732f3b15799cac6df1f971fa103
Christian Nytra <ch.nytra.13@gmail.com>
2025-10-06 23:02:44 +0200

    card bald

---
+++ app/src/main/java/com/client/mesomanager/ui/composables/cards/MesocycleCard.kt
@@ -0,0 +1,9 @@
+package com.client.mesomanager.ui.composables.cards
+
+import androidx.compose.runtime.Composable
+import com.client.mesomanager.data.entities.Mesocycle
+
+@Composable
+fun MesocycleCard(meso: Mesocycle){
+
+}
