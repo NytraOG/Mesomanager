d81c33d9d2ef9b7eb5d9669ab84a35c43304bef6
Christian Nytra <ch.nytra.13@gmail.com>
2025-10-19 15:59:30 +0200

    confign inc

---
+++ app/src/main/java/com/client/mesomanager/ui/screens/MuscleGroupConfigurationsScreen.kt
@@ -0,0 +1,20 @@
+package com.client.mesomanager.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.client.mesomanager.data.enums.MuscleGroup
+
+@Composable
+fun MuscleGroupConfigurationsScreen(
+    modifier: Modifier = Modifier,
+
+){
+
+}
+
+@Preview
+@Composable
+fun PreviewMuscleGroupConfigurationsScreen(){
+
+}
